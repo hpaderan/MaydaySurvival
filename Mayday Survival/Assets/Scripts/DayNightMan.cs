bd337c73a3fd60ea956ce0bd1fa8e8c9fae480c4
Harrold Paderan <harrold_paderan@yahoo.com>
2019-05-18 11:47:12 -0600

    Attempted Day/Night

--- Mayday Survival/Assets/Scripts/DayNightMan.cs
+++ Mayday Survival/Assets/Scripts/DayNightMan.cs
@@ -3,9 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightManager : MonoBehaviour
-{
+{    /* Harrold Paderan 18-May-2019 */
+
+    public float totalTime;
+    public float timeMultiplier;
+    public int hours;
+
     public bool isNight;
 
     public AudioSource dayMusic;
     public AudioSource nightMusic;
+
+    /*****************************************************************************************/
+    /* 18-May-2019 https://www.javaworld.com/article/2073352/core-java-simply-singleton.html */
+    private static DayNightManager instance = null;
+    protected DayNightManager() {
+        // Exists only to defeat instantiation.
+    }
+    public static DayNightManager getInstance(){
+        if ( instance == null )
+            instance = new DayNightManager();
+        
+        return instance;    }
+    /*****************************************************************************************/
+
+    private void Start()
+    {
+        totalTime = 0;
+        hours = 0;
+    }
+
+    private void Update()
+    {
+        totalTime += Time.deltaTime * timeMultiplier;
+        hours = (int) totalTime % 24;
+    }
+
+    public void ActivateNight ()
+    {
+        // Night time starts here
+    }
+
+    public void ActivateDay()
+    {
+        // Day time starts here
+    }
 }
