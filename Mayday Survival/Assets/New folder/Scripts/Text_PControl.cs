01b516ce51f61951c3a3a61fabc7f7c8783902cb
Kwan <timtimkwan@gmail.com>
2019-05-18 17:59:08 -0600

    This demonstrate how to implement the inventory system

---
+++ Mayday Survival/Assets/New folder/Scripts/Text_PControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text_PControl : MonoBehaviour
+{
+    public Interactable focus;
+    Camera cam;
+    public Item metal;
+        // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime,0f,Input.GetAxis("Vertical")*Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    //Set
+                }
+            }
+            //interactable
+        }
+        Inventory backpack = Inventory.instance;
+        if (Input.GetKeyUp(KeyCode.F)) {
+            //set invenotry [0] = this
+
+            backpack.ADD(metal);
+        }
+        if (Input.GetKeyUp(KeyCode.G)) {
+            //remove inventory[0]
+            backpack.Remove(metal);
+        }
+    }
+}
