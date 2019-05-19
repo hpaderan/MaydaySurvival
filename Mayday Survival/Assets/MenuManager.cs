8b4b6c1cfaae7b14cf20606cbb49328f14d4508c
Brennen <nijuuseikishounen@gmail.com>
2019-05-19 12:01:37 -0600

    Menu Scene Added
    Sets playerpref ints for large text and color blind mode, start button loads scene number 1 in the build settings

---
+++ Mayday Survival/Assets/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public Color NeutralColor;
+    public Color HoverColor;
+
+    public void ButtonEnter(Text ThisButton)
+    {
+        ThisButton.color = HoverColor;
+    }
+
+    public void ButtonExit(Text ThisButton)
+    {
+        ThisButton.color = NeutralColor;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ColorBlind(int TF)
+    {
+        PlayerPrefs.SetInt("CBlind", TF);
+    }
+
+    public void LargeText(int TF)
+    {
+        PlayerPrefs.SetInt("LText", TF);
+    }
+}
