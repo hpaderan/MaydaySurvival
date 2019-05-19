bc26457b75d7ddf176054d896a9b46a11720ee78
Ryan <shoelaces347@gmail.cm>
2019-05-19 09:52:15 -0600

    Merge looking good
    very nice

--- Mayday Survival/Assets/Scripts/EventSystem/ItemReward.cs
+++ Mayday Survival/Assets/Scripts/EventSystem/ItemReward.cs
@@ -9,7 +9,9 @@ public class ItemReward : Reward
     public override void Activate()
     {
         base.Activate();
-        GameObject go = Instantiate(item.gameObject);
-        //Inventory.instance.Add();
+        GameObject go = Instantiate(item.gameObject, FindObjectOfType<PlayerScript>().pickupPoint.position, Quaternion.identity);
+        go.transform.parent = FindObjectOfType<PlayerScript>().pickupPoint;
+        go.SetActive(false);
+        Inventory.instance.Add(item);
     }
 }
