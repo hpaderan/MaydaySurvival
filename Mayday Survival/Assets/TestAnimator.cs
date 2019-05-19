52419d9ec5f7698f9205929bf2ee6f1dc06ea40e
Brennen <nijuuseikishounen@gmail.com>
2019-05-18 21:22:40 -0600

    Character and animations added

---
+++ Mayday Survival/Assets/TestAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimator : MonoBehaviour
+{
+    public Animator Anim;
+    public bool MissionCompleteTest;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            //start running
+            Anim.SetTrigger("Running");
+        }
+
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            //pick up item
+            Anim.SetTrigger("PickUp");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //put down item
+            Anim.SetTrigger("PutDown");
+            //if mission complete, waves
+            Anim.SetBool("MissionComplete", true);
+            //else, goes idle
+            Anim.SetBool("MissionComplete", true);
+        }
+    }
+}
